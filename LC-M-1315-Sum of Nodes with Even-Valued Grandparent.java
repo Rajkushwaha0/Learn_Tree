b65d92da32f72af426cb262d6ce449f52e10a264
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2023-08-14 23:42:07 +0530

    Create LC-M-1315-Sum of Nodes with Even-Valued Grandparent.java

---
+++ LC-M-1315-Sum of Nodes with Even-Valued Grandparent.java
@@ -0,0 +1,23 @@
+class Solution {
+    int maxsum=0;
+    public int sumEvenGrandparent(TreeNode root) {
+        solve(root);
+        return maxsum;
+    }
+    public void solve(TreeNode root){
+        if(root==null) return ;
+        if(root.val%2==0){
+            if(root.left!=null){
+                if(root.left.left!=null) maxsum+=root.left.left.val;
+                if(root.left.right!=null) maxsum+=root.left.right.val;
+            }
+            if(root.right!=null){
+                if(root.right.left!=null) maxsum+=root.right.left.val;
+                if(root.right.right!=null) maxsum+=root.right.right.val;
+            }
+        }
+        solve(root.left);
+        solve(root.right);
+
+    }
+}
