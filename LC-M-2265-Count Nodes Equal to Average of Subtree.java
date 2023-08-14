dc29b2d7bbac9608e6e425d654ec3ce80fbc7b65
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2023-08-15 00:02:35 +0530

    Create LC-M-2265-Count Nodes Equal to Average of Subtree.java

---
+++ LC-M-2265-Count Nodes Equal to Average of Subtree.java
@@ -0,0 +1,28 @@
+class Pair{
+    int getKey;
+    int getValue;
+    public Pair(int a,int b){
+        this.getKey=a;
+        this.getValue=b;
+    }
+}
+class Solution {
+    int count;
+    public int averageOfSubtree(TreeNode root) {
+        solve(root);
+        return count;
+    }
+    public Pair solve(TreeNode root){
+        if(root==null) {
+            Pair p = new Pair(0,0);
+            return p;
+        }
+        Pair left1 = solve(root.left);
+        Pair right1 = solve(root.right);
+        int n= left1.getValue+right1.getValue+1;
+        int sum=root.val+left1.getKey+right1.getKey;
+        int avg = sum/n;
+        if(avg==root.val) count++;
+        return new Pair(sum,n);
+    }
+}
